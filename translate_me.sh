c119cd497afc66d10d94e0ce03dcc8b14afe5684
Robbie Minshall <rjminsha@us.ibm.com>
2015-05-28 09:11:39 -0400

    Adding in option to update machine translated files

--- translate_me.sh
+++ translate_me.sh
@@ -31,12 +31,13 @@ export lang_all='de,es,fr,it,ja,pt-BR,zh-Hans,zh-Hant'
 
 usage()
 {
-    echo "Usage: `basename $0` [-s source_pattern] [-o output_directory]"
+    echo "Usage: `basename $0` -s source_pattern -p project_name [-r CREATE|UPDATE] [-d] [-h]"
     echo ""
-    echo "Using IBM Globalization Service on Bluemix translates files that match source_pattern" 
+    echo "Using IBM Globalization Service on Bluemix this script translates files that match source_pattern" 
     echo ""
     echo "-s        Name of the Source file for translation.  Will look for this filename, if not found will search the directory tree for the file"
     echo "-p        Name of the Globalization project to create"
+    echo "-r        [default=create] stage to run.  create or update.  If create a project will be created if needed and strings will be downloaded, if update then manually translated strings will be updated in the Globalization Service"
     echo "-d        Debug mode"
     echo "-h        Displays this usage information"
     echo ""
@@ -89,15 +90,228 @@ wait_for_translation(){
     fi 
 }
 
+update_project_with_translated_files(){
+    echo "update_project_with_translated_files"
+    #########################################################
+    # Find and update all files that match input pattern    #
+    #########################################################
+    # check if this is a full path 
+    if [ -f ${INPUT_PATTERN} ]; then 
+        echo "found individual file ${INPUT_PATTERN}"
+        export source_files="$INPUT_PATTERN"
+        export single_file="TRUE"
+    else 
+        echo "searching directory structure for ${INPUT_PATTERN}"
+        export source_files=$(find `pwd` -name ${INPUT_PATTERN})
+        export single_file="FALSE"
+        echo "${source_files}"
+        if [ -z "${source_files}" ]; then 
+            echo -e "${red}Could not locate source file that matches ${INPUT_PATTERN} ${no_color}"
+            echo -e "${label_color}Directory contents: ${no_color}"
+            find . 
+            return 1
+        fi 
+    fi 
+
+    for file in $source_files; do
+        cur_dir=`pwd`
+        local_file_path="${file##${cur_dir}/}"
+        echo ""
+        echo ""
+        echo "Processing $local_file_path"
+        directory="${file%/*}"
+        if [ "${directory}" == "${file}" ]; then 
+            directory="."
+        fi 
+        debugme echo "directory of resources:$directory"
+        filename="${file##/*/}"
+        debugme echo "source filename:$filename" 
+        extension="${filename##*.}"
+        debugme echo "filetype:$extension"
+        case $extension in
+            'properties') filetype="java";;
+            'json') filetype="json";;
+            ?) 
+            echo -e "${red}Unrecognized file type $extension used";
+            return 1
+        esac
+
+        # find the naming pattern  
+        prefix="${filename%%_*}"
+        if [ -z "$prefix" ]; then
+            echo -e "${red}Non supported input.  Assuming the input is of type [prefix]_[lang].[type] ${no_color}"
+            return 1 
+        fi 
+        debugme echo "prefix:${prefix}"
+        source_lang="${filename#*_}"
+        source_lang="${source_lang%%.*}"
+        echo "source language:${source_lang}"
+
+        # Often translation centers send back non slightly different extensions than what the Globalization Service expects
+        if [ "${source_lang}" == "zh_Hans" ]; then 
+            source_lang="zh-Hans"
+        elif [ "${source_lang}" == "zh_Hant" ]; then 
+            source_lang="zh-Hant"
+        elif [ "${source_lang}" == "pt_BR" ]; then 
+            source_lang="pt-BR"
+        fi 
+        
+        archive_path="${directory##${cur_dir}/}"
+        if [ "${single_file}" == "TRUE" ]; then
+            archive_path="."
+            THIS_SUBMISSION_NAME=${SUBMISSION_NAME}
+        else 
+            THIS_SUBMISSION_NAME="${SUBMISSION_NAME}_${archive_path}"
+        fi 
+        
+        pushd . 
+        cd ${directory}
+
+        # upload translated file 
+        echo "Uploading ${directory}/${filename}"
+        java -cp "$GAAS_LIB/*" com.ibm.gaas.client.tools.cli.GaasCmd import -f ${file} -l ${source_lang} -t ${filetype} -p ${THIS_SUBMISSION_NAME} -u ${GAAS_ENDPOINT} -k ${GAAS_API_KEY}
+        local result=$?
+        if [ ${result} -ne 0 ]; then 
+            echo "" 
+            echo ""
+            echo -e "${label_color}Could not process ${directory}/${filename} ${no_color}"
+            echo "" 
+            echo ""
+        else 
+            echo -e "${green}Successfully processed ${local_file_path} ${no_color}"
+        fi 
+
+        # wait for translation to complete 
+        wait_for_translation ${THIS_SUBMISSION_NAME}  
+
+        popd
+    done 
+    return 0
+}
+
+create_project_download_files(){
+    ############################################################
+    # Find and translate all files that match input pattern    #
+    ############################################################
+    # check if this is a full path 
+    if [ -f ${INPUT_PATTERN} ]; then 
+        echo "found individual file ${INPUT_PATTERN}"
+        export source_files="$INPUT_PATTERN"
+        export single_file="TRUE"
+    else 
+        echo "searching directory structure for ${INPUT_PATTERN}"
+        export source_files=$(find `pwd` -name ${INPUT_PATTERN})
+        export single_file="FALSE"
+        echo "${source_files}"
+        if [ -z "${source_files}" ]; then 
+            echo -e "${red}Could not locate source file that matches ${INPUT_PATTERN} ${no_color}"
+            echo -e "${label_color}Directory contents: ${no_color}"
+            find . 
+            return 1
+        fi 
+    fi 
+
+    for file in $source_files; do
+        cur_dir=`pwd`
+        local_file_path="${file##${cur_dir}/}"
+        echo ""
+        echo ""
+        echo "Processing $local_file_path"
+        directory="${file%/*}"
+        if [ "${directory}" == "${file}" ]; then 
+            directory="."
+        fi 
+        debugme echo "directory of resources:$directory"
+        filename="${file##/*/}"
+        debugme echo "source filename:$filename" 
+        extension="${filename##*.}"
+        debugme echo "filetype:$extension"
+        case $extension in
+            'properties') filetype="java";;
+            'json') filetype="json";;
+            ?) 
+            echo -e "${red}Unrecognized file type $extension used";
+            return 1
+        esac
+
+        # find the naming pattern  
+        prefix="${filename%_*}"
+        if [ -z "$prefix" ]; then
+            echo -e "${red}Non supported input.  Assuming the input is of type [prefix]_[lang].[type] ${no_color}"
+            return 1 
+        fi 
+        debugme echo "prefix:${prefix}"
+        source_lang="${filename#*_}"
+        source_lang="${source_lang%%.*}"
+        if [ "${source_lang}" != "en" ]; then 
+            echo -e "${red}Currently only supports english as source language and not ${source_lang}${no_color}"
+            return 2
+        fi 
+        debugme echo "source language:${source_lang}"
+        debugme echo "${label_color}Processed files will be placed in ${directory} and will follow naming pattern:${prefix}_[lang].${extension} ${no_color}"
+        
+        archive_path="${directory##${cur_dir}/}"
+        if [ "${single_file}" == "TRUE" ]; then
+            archive_path="."
+            THIS_SUBMISSION_NAME=${SUBMISSION_NAME}
+        else 
+            THIS_SUBMISSION_NAME="${SUBMISSION_NAME}_${archive_path}"
+        fi 
+        echo "---------------------------------------------------------------------------------------"
+        echo "Checking/creating Globalization Project ${THIS_SUBMISSION_NAME} "
+        echo "---------------------------------------------------------------------------------------"
+        echo "Creating/checking for IBM Globalization Service project ${THIS_SUBMISSION_NAME}"
+        java -cp "$GAAS_LIB/*" com.ibm.gaas.client.tools.cli.GaasCmd create -p ${THIS_SUBMISSION_NAME} -u ${GAAS_ENDPOINT} -k ${GAAS_API_KEY} -s ${source_lang} -l ${lang_all}
+        RESULT=$?
+        if [ $RESULT -eq 1 ]; then
+            echo "..Project has been already created"
+        else 
+            echo "..Created project"
+        fi  
+        
+        pushd . 
+        cd ${directory}
+
+        # upload source 
+        echo "Uploading ${GAAS_SOURCE_FILE}"
+        java -cp "$GAAS_LIB/*" com.ibm.gaas.client.tools.cli.GaasCmd import -f ${file} -l ${source_lang} -t ${filetype} -p ${THIS_SUBMISSION_NAME} -u ${GAAS_ENDPOINT} -k ${GAAS_API_KEY}
+
+        # wait for translation to complete 
+        wait_for_translation ${THIS_SUBMISSION_NAME}  
+
+        # download translated files 
+        array=(${lang_all//,/ })
+
+        for i in "${!array[@]}"
+        do
+            OUTPUT_FILE_NAME="${prefix}_${array[i]}.${extension}"
+            echo "Downloading ${array[i]} translated file ${OUTPUT_FILE_NAME} into ${archive_path}"
+            java -cp "$GAAS_LIB/*" com.ibm.gaas.client.tools.cli.GaasCmd export -f ${OUTPUT_FILE_NAME} -l ${array[i]} -t ${filetype} -p ${THIS_SUBMISSION_NAME} -u ${GAAS_ENDPOINT} -k ${GAAS_API_KEY}
+            RESULT=$? 
+            if [ $RESULT -ne 0 ]; then
+                echo "Failed"
+                exit $RESULT
+            fi 
+            echo ""
+        done
+        cp "${prefix}_zh-Hans.${extension}" "${prefix}_zh-CN.${extension}"
+        cp "${prefix}_zh-Hant.${extension}" "${prefix}_zh-TW.${extension}"
+
+        echo -e "${green}Successfully processed ${local_file_path} ${no_color}"
+        popd
+    done 
+    return 0
+}
+
 ########################
 # Process arguments    #
 ########################
-
-while getopts "s:p:hd" OPTION
+while getopts "s:p:r:hd" OPTION
 do
     case $OPTION in
         s) export INPUT_PATTERN=$OPTARG; echo "set INPUT_PATTERN to ${OPTARG}";;
         p) export SUBMISSION_NAME=$OPTARG; echo "set SUBMISSION_NAME to ${OPTARG}";;
+        r) export JOB_TYPE=$OPTARG; echo "set JOB_TYPE to ${OPTARG}";;
         h) usage; exit 1;;
         d) usage; export DEBUG=1;;
         ?) 
@@ -112,6 +326,10 @@ print_limitations
 ###################
 # Check inputs    #
 ###################
+if [ -z $JOB_TYPE ]; then 
+    export JOB_TYPE="CREATE"    
+fi 
+
 if [ -z $GAAS_ENDPOINT ]; then 
     export GAAS_ENDPOINT="https://gaas.mybluemix.net/translate"
 fi 
@@ -119,6 +337,7 @@ if [ -z $GAAS_API_KEY ]; then
     echo -e "${red}API Key for Globalization Service must be set in the environment${no_color}"
     exit 1
 fi 
+
 if [ -z $SUBMISSION_NAME ]; then 
     echo -e "${red}SUBMISSION_NAME must be set in the environment${no_color}"
     exit 1
@@ -141,118 +360,31 @@ else
     echo "${INPUT_PATTERN} is the source file pattern"
 fi 
 
-####################################
-# Create Globalization Project     #
-####################################
+
 if [[ $DEBUG -eq 1 ]]; then
     set -x 
 fi 
 
-############################################################
-# Find and translate all files that match input pattern    #
-############################################################
-# check if this is a full path 
-if [ -f ${INPUT_PATTERN} ]; then 
-    echo "found individual file ${INPUT_PATTERN}"
-    export source_files="$INPUT_PATTERN"
+if [ "${JOB_TYPE}" == "UPDATE" ]; then 
+    echo "----------------------------------------------------"
+    echo "Updating Globalization project with translated files"
+    echo "----------------------------------------------------"
+
+   update_project_with_translated_files
+   result=$?
+elif [ "${JOB_TYPE}" == "CREATE" ]; then
+    echo "-----------------------------------------------------------------------------------------"
+    echo "Checking/creating Globalization project and uploading new strings for machine translation"
+    echo "-----------------------------------------------------------------------------------------"
+    create_project_download_files
+    result=$?
 else 
-    echo "searching directory structure for ${INPUT_PATTERN}"
-    export source_files=$(find `pwd` -name ${INPUT_PATTERN})
-    if [ -z $source_files ]; then 
-        echo -e "${red}Could not locate source file that matches ${INPUT_PATTERN} ${no_color}"
-        echo -e "${label_color}Directory contents: ${no_color}"
-        find . 
-        exit 1
-    fi 
+    echo "Job type must be either CREATE or UPDATE"
+    usage 
+    exit 1
 fi 
 
-COUNT=0
-for file in $source_files; do
-    cur_dir=`pwd`
-    local_file_path="${file##${cur_dir}/}"
-
-    echo "-----------------"
-    echo "Processing $local_file_path"
-    echo "-----------------" 
-    let COUNT+=1
-    if [ $COUNT -gt 1 ]; then 
-        THIS_SUBMISSION_NAME="${SUBMISSION_NAME}_${COUNT}"
-    else 
-        THIS_SUBMISSION_NAME=${SUBMISSION_NAME}
-    fi 
-    directory="${file%/*}"
-    debugme echo "directory of resources:$directory"
-    filename="${file##/*/}"
-    debugme echo "source filename:$filename" 
-    extension="${filename##*.}"
-    debugme echo "filetype:$extension"
-    case $extension in
-        'properties') filetype="java";;
-        'json') filetype="json";;
-        ?) 
-        echo -e "${red}Unrecognized file type $extension used";
-        exit 1
-    esac
-
-    # find the naming pattern  
-    prefix="${filename%_*}"
-    if [ -z "$prefix" ]; then
-        echo -e "${red}Non supported input.  Assuming the input is of type [prefix]_[lang].[type] ${no_color}"
-        exit 1 
-    fi 
-    debugme echo "prefix:${prefix}"
-    source_lang="${filename#*_}"
-    source_lang="${source_lang%%.*}"
-    if [ "${source_lang}" != "en" ]; then 
-        echo -e "${red}Currently only supports english as source language and not ${source_lang}${no_color}"
-        exit 2
-    fi 
-    debugme echo "source language:${source_lang}"
-    debugme echo "${label_color}Processed files will be placed in ${directory} and will follow naming pattern:${prefix}_[lang].${extension} ${no_color}"
-
-    echo "Creating/checking for IBM Globalization Service project ${THIS_SUBMISSION_NAME}"
-    java -cp "$GAAS_LIB/*" com.ibm.gaas.client.tools.cli.GaasCmd create -p ${THIS_SUBMISSION_NAME} -u ${GAAS_ENDPOINT} -k ${GAAS_API_KEY} -s ${source_lang} -l ${lang_all}
-    RESULT=$?
-
-    if [ $RESULT -eq 1 ]; then
-        echo "..Project has been already created"
-    else 
-        echo "..Created project"
-    fi  
-    
-    pushd . 
-    cd ${directory}
-    
-    # upload source 
-    echo "Uploading ${GAAS_SOURCE_FILE}"
-    java -cp "$GAAS_LIB/*" com.ibm.gaas.client.tools.cli.GaasCmd import -f ${file} -l ${source_lang} -t ${filetype} -p ${THIS_SUBMISSION_NAME} -u ${GAAS_ENDPOINT} -k ${GAAS_API_KEY}
-
-    # wait for translation to complete 
-    wait_for_translation ${THIS_SUBMISSION_NAME}  
-
-    # download translated files 
-    array=(${lang_all//,/ })
-    archive_path="${directory##${cur_dir}/}"
-    if [ "${archive_path}" == "${cur_dir}" ]; then
-        archive_path="."
-    fi 
-
-    for i in "${!array[@]}"
-    do
-        OUTPUT_FILE_NAME="${prefix}_${array[i]}.${extension}"
-        echo "Downloading ${array[i]} translated file ${OUTPUT_FILE_NAME} into ${archive_path}"
-        java -cp "$GAAS_LIB/*" com.ibm.gaas.client.tools.cli.GaasCmd export -f ${OUTPUT_FILE_NAME} -l ${array[i]} -t ${filetype} -p ${THIS_SUBMISSION_NAME} -u ${GAAS_ENDPOINT} -k ${GAAS_API_KEY}
-        RESULT=$? 
-        if [ $RESULT -ne 0 ]; then
-            echo "Failed"
-            exit $RESULT
-        fi 
-        echo ""
-    done
-    echo -e "${green}Successfully processed ${local_file_path} ${no_color}"
-    popd
-done 
-# attempt to find it
+# attempt to find dashboard
 export GAAS_DASHBOARD=$(cf service "IBM Globalization" | grep Dashboard | awk '{print $2}')
 debugme cf services
 debugme cf service "IBM Globalization"
@@ -269,14 +401,22 @@ if [ -z "$GAAS_DASHBOARD" ]; then
         echo -e "${red}Could not locate dashboard for service IBM Globalization${no_color}"
         export GAAS_DASHBOARD="unknown, please locate the service in your IBM Bluemix dashboard"
     fi 
-
 else
     debugme echo "found GAAS_DASHBOARD :${GAAS_DASHBOARD}"
 fi 
-echo -e "${label_color}All source files have been placed in the archive of this build, and can be used by additional stages${no_color}"
-echo -e "${label_color}All translated files have been put in the same directory as the original source files${no_color}"
-echo 
-echo -e "A typical continuous delivery scenario uses machine translation as a part of their continuous integration process.  Periodically, the translated messages can be reviewed and updated by translation experts.  This is best done directly within the Globalization Service Dashboard on IBM Bluemix.  Manually applied updates to translated strings will note be overwritten by future build processes unless the source string has also changed."  
+
+if [ $result -eq 0 ]; then
+    echo -e "${label_color}All source files have been placed in the archive of this build, and can be used by additional stages${no_color}"
+    echo -e "${label_color}All translated files have been put in the same directory as the original source files${no_color}"
+    echo 
+    echo -e "A typical continuous delivery scenario uses machine translation as a part of their continuous integration process.  Periodically, the translated messages can be reviewed and updated by translation experts.  This is best done directly within the Globalization Service Dashboard on IBM Bluemix.  Manually applied updates to translated strings will note be overwritten by future build processes unless the source string has also changed."  
+else 
+    echo -e "There are a number of ways that you can get help:"
+    echo -e "1. Post a question on ${label_color} https://developer.ibm.com/answers/ ${no_color} and 'Ask a question' with tags 'docker', 'containers' and 'devops-services'"
+    echo -e "2. Open a Work Item in our public devops project: ${label_color} https://hub.jazz.net/project/alchemy/Alchemy-Ostanes ${no_color}"
+    echo 
+    echo -e "You can also review and fork our sample scripts on ${label_color} https://github.com/Osthanes ${no_color}"
+fi 
 echo 
 echo -e "The Globalization Dashboard for this organization and space is located at ${green} ${GAAS_DASHBOARD} ${no_color}"
 
