8458ec361633e53d1985da38b43db800f4d9fac6
JACOB R. GARCOWSKI <jgarcows@us.ibm.com>
2015-12-14 13:47:28 -0500

    removing setenv_globalization from github

--- setenv_globalization.sh
+++
@@ -1,5 +0,0 @@
-export GAAS_ENDPOINT=https://gp-beta-rest.ng.bluemix.net/translate/rest
-export GAAS_INSTANCE_ID=8e10d61652309af6ba6a6196d351fb92
-export GAAS_USER_ID=d0a74f636580b919a0f9adf896157c4f
-export GAAS_PASSWORD=JslLiS36R9F2pdH54tzcjLAgRGCM7aJj
-export GAAS_DASHBOARD=https://gp-beta-dashboard.ng.bluemix.net?instance_id=8e10d61652309af6ba6a6196d351fb92&space_id=4786619b-6f08-430b-a7eb-2274e11d70dc&X-Bluemix-Authorization-Endpoint=https%3A%2F%2Fmccp.ng.bluemix.net%2Flogin&X-Bluemix-Authorization-Endpoint-Query=public%3Dtrue
