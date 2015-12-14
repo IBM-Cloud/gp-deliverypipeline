19f9322fe55ec4403f501047db368edb54d39afe
JACOB R. GARCOWSKI <jgarcows@us.ibm.com>
2015-12-14 13:45:30 -0500

    Updates for using the new Globalization Pipeline beta

--- setenv_globalization.sh
+++ setenv_globalization.sh
@@ -1,3 +1,5 @@
-export GAAS_API_KEY=420173ac-3237-4a5f-88f9-074f6d11871a
-export GAAS_ENDPOINT=https://gaas.mybluemix.net/translate
-export GAAS_DASHBOARD=https://gaas-dashboard.mybluemix.net/overview.wss?instance_id=4cfe1f04-634d-4030-baf4-08e4865a4b44&space_id=7ab89e09-ef4d-4820-8653-de5fc06752c8
+export GAAS_ENDPOINT=https://gp-beta-rest.ng.bluemix.net/translate/rest
+export GAAS_INSTANCE_ID=8e10d61652309af6ba6a6196d351fb92
+export GAAS_USER_ID=d0a74f636580b919a0f9adf896157c4f
+export GAAS_PASSWORD=JslLiS36R9F2pdH54tzcjLAgRGCM7aJj
+export GAAS_DASHBOARD=https://gp-beta-dashboard.ng.bluemix.net?instance_id=8e10d61652309af6ba6a6196d351fb92&space_id=4786619b-6f08-430b-a7eb-2274e11d70dc&X-Bluemix-Authorization-Endpoint=https%3A%2F%2Fmccp.ng.bluemix.net%2Flogin&X-Bluemix-Authorization-Endpoint-Query=public%3Dtrue
